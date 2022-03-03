5c75c5bedf85a7fb99c4ebaf6514647cbaf90eda
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-03-03 17:58:07 -0500

    Updated database to new a3 db in server.ts

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ import mongoose from "mongoose";
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.lskpe.mongodb.net";
+const HOST = "cluster0.az7ha.mongodb.net";
 const DB_NAME = "tuiter";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
